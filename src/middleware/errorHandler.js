b910b8e9c1862e92f2c94cb7f8471945220520f2
Raj Sharma <raj@shopflow.com>
2026-04-18 09:53:45 +0530

    new login logic applied

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,10 @@
 // Global error handler middleware
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
+<<<<<<< HEAD
   res.status(500).json({ error: 'Server Error Occured', message: err.message });
+=======
+  res.status(500).json({ error: 'Unexpected application error', message: err.message });
+>>>>>>> 9d788df (fix(error):clarify error message for client)
 };
 module.exports = errorHandler;
