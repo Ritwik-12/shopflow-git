7499365567d45838427baa4e7d51c018f9e07bbd
Raj Sharma <raj@shopflow.com>
2026-04-19 19:15:03 +0530

    new error message accepted

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,10 +1,6 @@
 // Global error handler middleware
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
-<<<<<<< HEAD
-  res.status(500).json({ error: 'Server Error Occured', message: err.message });
-=======
   res.status(500).json({ error: 'Unexpected application error', message: err.message });
->>>>>>> 9d788df (fix(error):clarify error message for client)
 };
 module.exports = errorHandler;
